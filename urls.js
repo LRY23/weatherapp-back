25a583e5283e8ee3824335223ca94013da1e9772
Yann <yannleroy23@gmail.com>
2023-07-11 08:22:25 +0200

    ok

--- urls.js
+++ urls.js
@@ -1,7 +1,7 @@
 // Insert the backend URL given by Vercel in this variable
-const BACKEND_URL = 'https://weatherapp-backend.vercel.app/';
+const BACKEND_URL = 'http://weatherapp-back.vercel.app';
 
 // Insert the frontend URL given by Vercel in this variable
-const FRONTEND_URL = 'https://weatherapp-frontend.vercel.app/';
+const FRONTEND_URL = 'https://weatherapp-front-sepia.vercel.app/';
 
 module.exports = { BACKEND_URL, FRONTEND_URL }; // Do not edit/remove this line
